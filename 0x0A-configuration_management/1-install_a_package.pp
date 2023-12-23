c2f10e21a4e7c29dfd65ca12c7bf3d0fd0a86b9a
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2023-12-22 21:01:28 -0800

    Add comments

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,17 @@
 #!/usr/bin/pup
-# Using Puppet to install flask from pip3
 
+# Install python-pip package
 package {'python3-pip':
   ensure  => present,
 }
 
+# Install werkzeug package
 package {'werkzeug':
   ensure   => '2.1.1',
   provider =>'pip3',
 }
 
+# Install flask package
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
