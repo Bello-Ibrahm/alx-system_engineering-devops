00e8aa350a6b2f10e13e5d0cb4945bc0e7130fe1
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2023-12-22 20:57:16 -0800

    fix checker err #3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,16 @@
 #!/usr/bin/pup
 # Using Puppet to install flask from pip3
 
+package {'python3-pip':
+  ensure  => present,
+}
+
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider =>'pip3',
+}
+
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
