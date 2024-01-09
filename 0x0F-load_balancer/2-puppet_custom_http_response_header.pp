9500ecdf0377810db48607f2a0be2cf381127934
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-01-09 03:57:57 -0800

    Remove empty space

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,4 +3,3 @@ exec { 'command':
   command  => 'apt-get -y update; apt-get -y install nginx; sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default; service nginx restart',
   provider => shell,
 }
-
