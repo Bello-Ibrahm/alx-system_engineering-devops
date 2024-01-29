7df337b1d7637de5b694e0ccefa3043d705d8750
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-01-29 00:18:44 -0800

    Refactor code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-""" """
+""" Script that uses REST API for a given employee ID
+and returns information about the employee TODO list progress."""
 import requests
 from sys import argv
 
 
-def getEmployee():
-    url = 'https://jsonplaceholder.typicode.com/'
+def get_employee():
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    user_id = argv[1]
 
-    user_res = requests.get(url + 'users/{}'.format(argv[1])).json()
+    user_res = requests.get(base_url + 'users/{}'.format(user_id)).json()
     user_name = user_res.get('name')
 
-    todos_res = requests.get(url + 'todos').json()
+    todos_res = requests.get(base_url + 'todos').json()
 
     titles = []
     completed = 0
     total = 0
 
     for t in todos_res:
-        if (t['userId'] == int(argv[1])):
+        if (t['userId'] == int(user_id)):
             if (t['completed'] is True):
                 completed += 1
                 titles.append(t['title'])
@@ -27,8 +29,8 @@ def getEmployee():
           .format(user_name, completed, total))
 
     for title in titles:
-        print("\t {}".format(title))
+        print("\t{}".format(title))
 
 
 if __name__ == "__main__":
-    getEmployee()
+    get_employee()
