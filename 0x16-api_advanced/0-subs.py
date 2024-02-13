7467c12d112bf6511db0f8fe7f71a8aab096002e
an-nur-info-tech <wasiubello60@gmail.com>
2024-02-13 20:32:02 +0100

    Fix checker issue #7

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Get number of subscribers for a subreddit """
-    headers = {'User-Agent': '0x16-api_advanced:project:\
-    v1.0.0 (by /u/Moh\'d Bello Ibrahim'}
-    count = requests.get('https://www.reddit.com/r/{}/about.json'.format(
-        subreddit), headers=headers).json()
     try:
-        return count.get('data', {}).get('subscribers', 0)
+        headers = {'User-Agent': '0x16-api_advanced:project:\
+        v1.0.0 (by /u/Moh\'d Bello Ibrahim'}
+        count = requests.get('https://www.reddit.com/r/{}/about.json'.format(
+                             subreddit), headers=headers)
+        count.raise_for_status()
+        return count.json().get('data', {}).get('subscribers', 0)
     except Exception:
         return 0
