2521a5091a3b88be99ae304313d22b124ee3140e
an-nur-info-tech <wasiubello60@gmail.com>
2024-02-13 11:02:00 +0100

    Initial commit with task 0. How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Advance API module """
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """ Get number of subscribers for a subreddit """
+    header = {
+            'User-Agent': 'Moh\'d Bello Ibrahim'
+            }
+    count = get('https://www.reddit.com/r/{}/about.json'.format(
+        subreddit), headers=header).json()
+    try:
+        return count.get('data').get('subscribers')
+    except:
+        return 0
