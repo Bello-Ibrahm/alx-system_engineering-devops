b3c8993c970f04f20ea09557f7fe84f43caecd6e
Bello-Ibrahm <Ibrahimbello1243@gmail.com>
2024-02-22 10:02:22 +0100

    Fix checker requirement #5

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 # Increases the amount of traffic an Nginx server can handle.
 
 # Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+exec { 'set-limit-to-2000':
+  command => 'sed -i "s/15/2000/" /etc/default/nginx',
   path    => ['/usr/local/bin', '/bin']
-} 
+} ->
 
 # Restart Nginx
 exec { 'nginx-restart':
