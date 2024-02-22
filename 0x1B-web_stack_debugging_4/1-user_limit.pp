9ea036c88e37f0b99158bf79470f4ec5dc41de8c
Bello-Ibrahm <Ibrahimbello1243@gmail.com>
2024-02-22 10:11:00 +0100

    Fix checker error #2

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,12 @@
 
 # Increase hard file limit for holberton user
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  command => 'sed -i "s/5/5000/" /etc/security/limits.conf',
   path    => ['/usr/local/bin', '/bin']
 } ->
 
 # Increase soft file limit for holberton user
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/40000/" /etc/security/limits.conf',
+  command => 'sed -i "s/4/4000/" /etc/security/limits.conf',
   path    => ['/usr/local/bin', '/bin']
 }
