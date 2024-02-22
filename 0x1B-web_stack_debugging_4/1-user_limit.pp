ea363d7678eac8306ee4a27d3d8feb3624a08d99
Bello-Ibrahm <Ibrahimbello1243@gmail.com>
2024-02-22 10:11:47 +0100

    Fix checker error #3

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,11 @@
 # Enables the user holberton to login with file access right.
 
 # Increase hard file limit for holberton user
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "s/5/5000/" /etc/security/limits.conf',
-  path    => ['/usr/local/bin', '/bin']
-} ->
-
-# Increase soft file limit for holberton user
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "s/4/4000/" /etc/security/limits.conf',
-  path    => ['/usr/local/bin', '/bin']
+exec { 'hard limit':
+  command => "sed -i 's/5/4000/' /etc/security/limits.conf",
+  path    => '/bin'
+}
+exec { 'soft limit':
+  command => "sed -i 's/4/2000/' /etc/security/limits.conf",
+  path    => '/bin'
 }
