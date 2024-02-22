1a87e3f0409aadc9c2ba78c6aaa24b41617b6cc3
Bello-Ibrahm <Ibrahimbello1243@gmail.com>
2024-02-22 10:13:05 +0100

    Fix checker requirement #8

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,8 +5,6 @@ exec { 'set-limit-to-2000':
   command => 'sed -i "s/15/2000/" /etc/default/nginx',
   path    => ['/usr/local/bin', '/bin']
 } ->
-
-# Restart Nginx
 exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
